f6bc60bcd0d15ffc543b0f9a1346f490b1807dd7
AleksanderOMD <alexvalkoinenomd@gmail.com>
2024-02-14 17:43:34 -0600

    Add project files.

---
+++ OrexMaxBets/MenuVariables/BetSettings.cs
@@ -0,0 +1,44 @@
+﻿using MCM.Abstractions.FluentBuilder;
+using MCM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrexMaxBets.MenuVariables
+{
+    internal class BetSettings : IDisposable
+    {
+        private static BetSettings? _instance;
+        public static BetSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BetSettings();
+                }
+                return _instance;
+            }
+        }
+        public int MaxBet { get; set; } = 250;
+        public void Settings()
+        {
+            var builder = BaseSettingsBuilder.Create(SubModule.ModName, "Orex Max Bets")!
+                .SetFormat("xml")
+                .SetFolderName(SubModule.ModuleFolderName)
+                .SetSubFolder(String.Empty)
+                .CreateGroup("Tournament Bet Amount", groupBuilder => groupBuilder
+                .AddInteger("maxBest", "Max Bet", 250, 10000, new ProxyRef<int>(() => MaxBet, o => MaxBet = o), integerBuilder => integerBuilder
+                    .SetHintText("This is the maximum bet value for tournaments")));
+
+            var globalSettings = builder.BuildAsGlobal();
+            globalSettings.Register();
+        }
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ OrexMaxBets/SubModule.cs
@@ -0,0 +1,87 @@
+﻿using JetBrains.Annotations;
+using SandBox.Tournaments;
+using SandBox.Tournaments.MissionLogics;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+using OrexMaxBets.MenuVariables;
+
+namespace OrexMaxBets
+{
+    public class SubModule : MBSubModuleBase
+    {
+        //MCM things
+        public static readonly string ModuleFolderName = "OrexMaxBets";
+        public static readonly string ModName = "Orex Max Bets";
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harm = new Harmony("OrexMaxBets");
+            harm.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+        
+        
+        protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
+        {
+            base.InitializeGameStarter(game, starterObject);
+            
+        }
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            base.OnMissionBehaviorInitialize(mission);
+        }
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            BetSettings.Instance.Settings();
+        }
+    }
+    /*
+     * Upon inspection of the decompiled source Tournament behaviors are not handled like normal game behaviors, so overriding the class doesn't work, Leaving only the option of patching the method with Harmony, or doing the real bad and modifying the actual TaleWorlds Binaries
+     * I have no intention on modifying the Binaries to make this work, so patching with Harmony it is.
+     * */
+
+    [HarmonyPatch(typeof(TournamentBehavior), "GetMaximumBet")]
+    class patchBets
+    {
+        int MaxBet = 10000;
+        public static bool Prefix(ref int __result)
+        {
+            int num = BetSettings.Instance.MaxBet; 
+            if (Hero.MainHero.GetPerkValue(DefaultPerks.Roguery.DeepPockets))
+            {
+                num *= (int)DefaultPerks.Roguery.DeepPockets.PrimaryBonus;
+            }
+            __result = num;
+            return false;
+        }
+    }
+
+    //public class ModdedTournamentBehavior : TournamentBehavior
+    //{
+    //    public int MaxBetSetting = 10000;
+    //    public ModdedTournamentBehavior(TournamentGame tournamentGame, Settlement settlement, ITournamentGameBehavior gameBehavior, bool isPlayerParticipating) : base(tournamentGame, settlement, gameBehavior, isPlayerParticipating)
+    //    {
+
+    //    }
+    //    public new int GetMaximumBet()
+    //    {
+    //        int maxBet = MaxBetSetting;
+    //        if (Hero.MainHero.GetPerkValue(DefaultPerks.Roguery.DeepPockets))
+    //        {
+    //            maxBet *= (int)DefaultPerks.Roguery.DeepPockets.PrimaryBonus;
+    //        }
+    //        return maxBet;
+    //    }
+    //}
+}
